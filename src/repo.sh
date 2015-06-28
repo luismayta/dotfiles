27ee12ae28852d90139cd76f5906e54e95912702
Lucho <lucho@hadenlabs.com>
2015-06-28 15:04:25 -0500

    fix: values messages info, debug, success and error

--- src/repo.sh
+++ src/repo.sh
@@ -4,7 +4,7 @@ upgrade_repo() {
       msg "trying to update $1"
 
       if [ "$1" = "$app_name" ]; then
-          cd "$HOME/.$app_name" &&
+          cd "$path_repo" &&
           git pull origin "$git_branch"
       fi
 
@@ -15,14 +15,13 @@ upgrade_repo() {
 
 clone_repo() {
     program_exists "git" "Sorry, we cannot continue without GIT, please install it first."
-    endpath="$HOME/.$app_name"
 
-    if [ ! -e "$endpath/.git" ]; then
-        git clone --recursive -b "$git_branch" "$git_uri" "$endpath"
+    if [ ! -e "$path_repo/.git" ]; then
+        git clone --recursive -b "$git_branch" "$git_uri" "$path_repo"
         ret="$?"
         success "$1"
         debug
     else
-        upgrade_repo "$app_name"    "Successfully updated $app_name"
+        upgrade_repo "$app_name" "Successfully updated $app_name"
     fi
 }
