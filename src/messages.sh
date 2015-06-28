8aececd92d8797a1dbdc89d0bc5e8df69d602b6c
@slovacus <slovacus@gmail.com>
2015-06-28 12:38:02 -0500

    [feature/install] added files model

---
+++ src/messages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+    msg "\e[32m[✔]\e[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\e[31m[✘]\e[0m ${1}${2}"
+    exit 1
+}
+
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+      msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
