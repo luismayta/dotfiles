d942d81f496ee8597f49c159713b8dedc28126f2
Luis Mayta <luis@hadenlabs.com>
2015-06-30 15:06:45 -0500

    fix: install fonts install

--- src/load.sh
+++ src/load.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-for file in "$path_repo/"src/{config.sh,messages.sh,repo.sh,functions.sh}; do
+APP_NAME='.dotfiles'
+PATH_REPO="$HOME/$APP_NAME"
+
+for file in "$PATH_REPO/"src/{config.sh,messages.sh,repo.sh,functions.sh}; do
 	[ -r "$file" ] && source "$file"
 done
 unset file
