f426e3e8a606e2f4dd9caab56cf8f952f12bff44
@slovacus <slovacus@gmail.com>
2015-07-04 08:29:39 -0500

    [feature/#2] fixed format syntax code

--- install.sh
+++ install.sh
@@ -9,29 +9,29 @@ PATH_REPO="$HOME/$APP_NAME"
 PATH_BACKUP="$HOME/backup"
 FILES_FONTS="$PATH_REPO/resources/fonts/*"
 
-msg() {
+function msg() {
     printf '%b\n' "$1" >&2
 }
 
-success() {
+function success() {
     if [ "$ret" -eq '0' ]; then
         msg "\e[32m[✔]\e[0m ${1}${2}"
     fi
 }
 
-error() {
+function error() {
     msg "\e[31m[✘]\e[0m ${1}${2}"
     exit 1
 }
 
-debug() {
-    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+function debug() {
+    if [ "$DEBUG_MODE" -eq '1' ] && [ "$ret" -gt '1' ]; then
       msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
 
 ############################  BASIC SETUP TOOLS
-program_exists() {
+function program_exists() {
     local ret='0'
     type $1 >/dev/null 2>&1 || { local ret='1'; }
 
@@ -41,7 +41,7 @@ program_exists() {
     fi
 }
 
-clone_repo() {
+function clone_repo() {
     if [ ! -e "$PATH_REPO/.git" ]; then
         git clone --recursive -b "$GIT_BRANCH" "$GIT_URI" "$PATH_REPO"
         ret="$?"
