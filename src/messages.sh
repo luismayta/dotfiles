642aa78cd15ac5478a32cb33f9d84052a82368b1
Lucho <lucho@hadenlabs.com>
2015-07-04 08:29:39 -0500

    style: format syntax code (#2)

--- src/messages.sh
+++ src/messages.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-msg() {
+function msg() {
     printf '%b\n' "$1" >&2
 }
 
-success() {
+function success() {
     if [ "$ret" -eq '0' ]; then
         msg "\e[32m[✔]\e[0m ${1}${2}"
     fi
 }
 
-error() {
+function error() {
     msg "\e[31m[✘]\e[0m ${1}${2}"
     exit 1
 }
 
-debug() {
-    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+function debug() {
+    if [ "$DEBUG_MODE" -eq '1' ] && [ "$ret" -gt '1' ]; then
       msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
