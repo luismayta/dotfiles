dbc95bd37a6fbb935b97248ade640d70d4d84e9a
@slovacus <slovacus@gmail.com>
2015-07-12 23:09:25 -0500

    chore: implement code install pyenv (#9)

---
+++ tools/pyenv/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+APP_NAME='.dotfiles'
+PATH_REPO="$HOME/$APP_NAME"
+
+[ -r "$PATH_REPO/src/load.sh" ] && source "$PATH_REPO/src/load.sh"
+
+cat <<EOF
+
+-------------------
+ Pyenv Application
+-------------------
+
+EOF
+
+if [[ ! -e $PATH_PYENV ]]; then
+    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+fi
+
+
+cat <<EOF
+
+--------------------------
+ Fonts installed! Enjoy!
+--------------------------
+
+EOF
