096d1bc0956777fa2f7cace877ffb2cec496eb54
Lucho <lucho@hadenlabs.com>
2015-07-13 08:06:17 -0500

    chore: implement install gvm (#11)

---
+++ tools/gvm/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+APP_NAME='.dotfiles'
+PATH_REPO="$HOME/$APP_NAME"
+
+[ -r "$PATH_REPO/src/load.sh" ] && source "$PATH_REPO/src/load.sh"
+
+cat <<EOF
+
+--------------------
+ Go Version Manager
+--------------------
+
+EOF
+
+if [[ ! -e $PATH_GVM ]]; then
+    bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+fi
+
+cat <<EOF
+
+--------------------------
+ Gvm installed! Enjoy!
+--------------------------
+
+EOF
