eac0c1d5ae24450461b284603dbbdde6305c5044
Lucho <lucho@hadenlabs.com>
2015-07-13 07:57:36 -0500

    chore: implement function install fonts (#11)

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,18 @@ function install_pyenv(){
     "$PATH_REPO/tools/pyenv/install.sh"
 }
 
+function install_fonts(){
+    "$PATH_REPO/tools/fonts/install.sh"
+}
+
 function do_it(){
     for app in {zsh,git,tmux}; do
         program_exists "$app"
     done
     unset app
+
+    install_pyenv
+    install_fonts
 
     for path in "$PATH_REPO/"conf/{shell,app}; do
         for file_path in "$path/"*; do
@@ -25,10 +32,6 @@ function do_it(){
         unset file_path
     done
     unset path
-
-    msg "Install Fonts"
-
-    "$PATH_REPO/tools/fonts/install.sh"
 
     msg "Copying file bashrc adding it to ~/.zshrc"
 
