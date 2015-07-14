ad12c478c63dbe7191b2e18a6410019ebcc9b850
@slovacus <slovacus@gmail.com>
2015-07-13 23:40:28 -0500

    [feature/#12] fixed syntax function

--- install.sh
+++ install.sh
@@ -13,25 +13,25 @@ msg() {
     printf '%b\n' "$1" >&2
 }
 
-function success() {
+success() {
     if [ "$ret" -eq '0' ]; then
         msg "\e[32m[✔]\e[0m ${1}${2}"
     fi
 }
 
-function error() {
+error() {
     msg "\e[31m[✘]\e[0m ${1}${2}"
     exit 1
 }
 
-function debug() {
+debug() {
     if [ "$DEBUG_MODE" -eq '1' ] && [ "$ret" -gt '1' ]; then
       msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
 
 ############################  BASIC SETUP TOOLS
-function program_exists() {
+program_exists() {
     local $app=$1
     local message="Need to install $app."
     local ret='0'
@@ -44,7 +44,7 @@ function program_exists() {
     fi
 }
 
-function clone_repo() {
+clone_repo() {
     if [ ! -e "$PATH_REPO/.git" ]; then
         git clone --recursive -b "$GIT_BRANCH" "$GIT_URI" "$PATH_REPO"
         ret="$?"
