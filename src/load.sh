96b3fe3c1aaca8dc7a276117636f15ed9a269ce1
@slovacus <slovacus@gmail.com>
2015-07-23 16:02:34 -0500

    [#4] fixed load directory

--- src/load.sh
+++ src/load.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ ! $APP_NAME ]]; then
-    APP_NAME='.dotfiles'
-fi
-
-if [[ ! $PATH_REPO ]]; then
-    PATH_REPO="$HOME/$APP_NAME"
-fi
-
-echo $ROOT
-echo $PATH_REPO
-
-for file in "$PATH_REPO/"src/{config.sh,messages.sh,repo.sh,functions.sh}; do
-    echo $file
+for file in "$ROOT/"src/{config.sh,messages.sh,repo.sh,functions.sh}; do
 	[ -r "$file" ] && source "$file"
 done
 unset file
