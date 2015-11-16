f005f24448fc03db2ddb768e0a3461238947022e
@slovacus <slovacus@gmail.com>
2015-11-15 23:31:33 -0500

    [feature/#17] Added install git-extras

---
+++ tools/git-extras/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+cat <<EOF
+
+--------------------
+    Git Extras
+--------------------
+
+EOF
+
+curl -sSL http://git.io/git-extras-setup | sudo bash /dev/stdin
+
+cat <<EOF
+
+--------------------------
+ Git Extras installed! Enjoy!
+--------------------------
+
+EOF
