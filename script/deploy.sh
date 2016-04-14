a32648d03a513d04f37595988a539794084e19a1
Lucho <lucho@hadenlabs.com>
2016-04-14 05:22:36 -0500

    chore: added script utilities (#53)

---
+++ script/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+echo "execute Deploy"
+
+FILE_PLAYBOOK="$PROVISION_DIR/deploy.yml"
+
+ansible-playbook $FILE_PLAYBOOK -i $PATH_INVENTORY  -v \
+                 --user=$USER  --private-key=$PRIVATE_KEY \
+                --extra-vars $EXTRA_VARS
