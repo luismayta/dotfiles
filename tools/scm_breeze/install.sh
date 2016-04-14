2777bced0c391588ef880638e546be61130ab95f
Lucho <lucho@hadenlabs.com>
2016-04-14 08:22:38 -0500

    fix: not found install scm breeze (#56)

--- tools/scm_breeze/install.sh
+++ tools/scm_breeze/install.sh
@@ -9,6 +9,8 @@ cat <<EOF
 
 EOF
 
+[ -r "$SRC_DIR/load.sh" ] && source "$SRC_DIR/load.sh"
+
 if [[ ! -e $PATH_SCM_BREEZE ]]; then
     git clone git://github.com/ndbroadbent/scm_breeze.git $PATH_SCM_BREEZE
     $PATH_SCM_BREEZE/install.sh
