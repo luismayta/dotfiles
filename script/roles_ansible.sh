d3b16e804ddaee6a181c53bab92da5f3e31a5731
@slovacus <slovacus@gmail.com>
2016-05-26 18:31:11 -0500

    chore: download dependences roles ansible (#72)

--- script/roles_ansible.sh
+++ script/roles_ansible.sh
@@ -3,5 +3,5 @@
 
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
-ansible-galaxy install -r roles.txt \
+ansible-galaxy install -r requirements.yml \
                --roles-path $PROVISION_DIR/roles/contrib --force
