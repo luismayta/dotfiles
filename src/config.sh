a415dbbfec974d95c13605c90911f5330ebfb230
Lucho <lucho@hadenlabs.com>
2016-06-12 01:10:06 -0500

    fix: export var path (#77)

--- src/config.sh
+++ src/config.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-APP_NAME='.dotfiles'
-GIT_URI='https://github.com/luismayta/dotfiles.git'
-GIT_BRANCH='master'
-DEBUG_MODE='0'
-PATH_REPO="$HOME/$APP_NAME"
-PATH_BACKUP="$HOME/backup"
-PATH_PYENV="$HOME/.pyenv"
-PATH_GVM="$HOME/.gvm"
+export APP_NAME='.dotfiles'
+export GIT_URI='https://github.com/luismayta/dotfiles.git'
+export GIT_BRANCH='master'
+export DEBUG_MODE='0'
+export PATH_REPO="$HOME/$APP_NAME"
+export PATH_BACKUP="$HOME/backup"
+export PATH_PYENV="$HOME/.pyenv"
+export PATH_GVM="$HOME/.gvm"
 
-PATH_GIT_EXTRAS="$HOME/.gvm"
-PATH_RVM="$HOME/.rvm"
-PATH_NVM="$HOME/.nvm"
-PATH_TPM="$HOME/.tmux/plugins/tpm"
-PATH_THEMEPACK="$HOME/.tmux-themepack"
-PATH_SCM_BREEZE="$HOME/.scm_breeze"
-PATH_FONTS_REPO="$PATH_REPO/resources/fonts"
+export PATH_GIT_EXTRAS="$HOME/.gvm"
+export PATH_RVM="$HOME/.rvm"
+export PATH_NVM="$HOME/.nvm"
+export PATH_TPM="$HOME/.tmux/plugins/tpm"
+export PATH_THEMEPACK="$HOME/.tmux-themepack"
+export PATH_SCM_BREEZE="$HOME/.scm_breeze"
+export PATH_FONTS_REPO="$PATH_REPO/resources/fonts"
 
-FILE_SETTINGS_OSX="$SRC_DIR/settings/osx.sh"
-FILE_SETTINGS_LINUX="$SRC_DIR/settings/linux.sh"
+export FILE_SETTINGS_OSX="$SRC_DIR/settings/osx.sh"
+export FILE_SETTINGS_LINUX="$SRC_DIR/settings/linux.sh"
 
 if [[ `uname` == 'Darwin' ]]; then
 	[ -r "$FILE_SETTINGS_OSX" ] && source "$FILE_SETTINGS_OSX"
