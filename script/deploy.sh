3fd15d3adff10603e9650cd9d1350411cbb2e32e
@slovacus <slovacus@gmail.com>
2016-07-03 02:44:17 -0500

    fix: code deploy (#79)

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,6 +7,6 @@ echo "execute Deploy"
 
 FILE_PLAYBOOK="$PROVISION_DIR/deploy.yml"
 
-ansible-playbook $FILE_PLAYBOOK -i $PATH_INVENTORY  -v \
-                 --user=$USER  --private-key=$PRIVATE_KEY \
-                --extra-vars $EXTRA_VARS
+ansible-playbook "$FILE_PLAYBOOK" -i "$PATH_INVENTORY"  -v \
+                --user="$USER"  --private-key="$PRIVATE_KEY" \
+                --extra-vars "$EXTRA_VARS"
