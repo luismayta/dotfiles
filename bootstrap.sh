1960b6c90bd46d86c902c1bd3e9e2f93ecee73f4
Lucho <lucho@hadenlabs.com>
2016-07-23 22:51:09 -0500

    fix: install apps (#82)

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,26 +6,20 @@ export PROJECT_NAME=dotfiles
 export APP_DIR="$HOME/.$PROJECT_NAME"
 export SRC_DIR="$APP_DIR/src"
 
+# shellcheck source=src/load.sh
 [ -r "$SRC_DIR/load.sh" ] && source "$SRC_DIR/load.sh"
 
 function install_apps(){
-    "$TOOLS_DIR/pyenv/install.sh"
-    "$TOOLS_DIR/gvm/install.sh"
-    "$TOOLS_DIR/wakatime/install.sh"
-    "$TOOLS_DIR/git-extras/install.sh"
-    "$TOOLS_DIR/fonts/install.sh"
-    "$TOOLS_DIR/tpm/install.sh"
-    "$TOOLS_DIR/nvm/install.sh"
-    "$TOOLS_DIR/rvm/install.sh"
-    "$TOOLS_DIR/antibody/install.sh"
-    "$TOOLS_DIR/scm_breeze/install.sh"
-    "$TOOLS_DIR/tmux-themepack/install.sh"
+    for app in $APPS; do
+        "$TOOLS_DIR/${app}/install.sh"
+    done
+    unset app
 }
 
 function replace_files(){
     echo -n "This may overwrite existing files in your home directory. Are you sure? (y/n) "
 
-    read response
+    read -r response
 
     if [[ $response =~ ^[Yy]$ ]]; then
         initialize
@@ -56,7 +50,7 @@ function initialize(){
 clone_repo      "Successfully cloned $APP_NAME"
 
 msg             "\nThanks for installing $APP_NAME."
-msg             "© `date +%Y` $APP_NAME,s"
+msg             "© $(date +%Y) $APP_NAME,s"
 
 if [[ $TEST = 'true' ]]; then
     initialize
--- src/settings/linux.sh
+++ src/settings/linux.sh
@@ -2,4 +2,8 @@
 # -*- coding: utf-8 -*-
 
 export FONTS_DIR="$HOME/.fonts"
-mkdir -p $FONTS_DIR
+mkdir -p "$FONTS_DIR"
+export APPS=("pyenv" "gvm" "wakatime" "git-extras"
+             "fonts" "tpm" "nvm" "rvm" "antibody"
+             "scm_breeze" "tmux-themepack"
+            )
