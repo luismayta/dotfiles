bf9ec5da7539c7b99cb25d909f28efa4c4d8eb42
Lucho <lucho@hadenlabs.com>
2016-07-24 02:26:23 -0500

    chore: tools homebrew (#84)

---
+++ tools/homebrew/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# shellcheck source=src/load.sh
+[ -r "$SRC_DIR/load.sh" ] && source "$SRC_DIR/load.sh"
+
+cat <<EOF
+
+-------------------------
+ HomeBrew Version Manager
+-------------------------
+
+EOF
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+cat <<EOF
+
+---------------------------
+ HomeBrew installed! Enjoy!
+---------------------------
+
+EOF
