8722ff093028f599f4ff6fcf2cc69811987682a2
@slovacus <slovacus@gmail.com>
2016-09-24 10:49:59 -0500

    [#92] Fix: load shell

--- script/clean.sh
+++ script/clean.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+# shellcheck source=src/load.sh
+[ -r "src/load.sh" ] && source "src/load.sh"
 
-find $ROOT_DIR/ -name "*.pyc" -delete
-find $ROOT_DIR/ -name "*.swp" -delete
+find "${ROOT_DIR}/" -name "*.pyc" -delete
+find "${ROOT_DIR}/" -name "*.swp" -delete
+find "${ROOT_DIR}/" -name "__pycache__" -delete
--- script/provision.sh
+++ script/provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 echo "execute Provision"
