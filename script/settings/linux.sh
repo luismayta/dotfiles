74daaa89800e00450675d9517cd979dee994c37a
@slovacus <slovacus@gmail.com>
2016-09-27 16:20:47 -0500

    fix: clean exports vars (#92)

--- script/settings/linux.sh
+++ script/settings/linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-export FONTS_DIR="$HOME/.fonts"
+FONTS_DIR="$HOME/.fonts"
 mkdir -p "$FONTS_DIR"
-export APPS=("pyenv" "gvm" "git-extras"
+APPS=("pyenv" "gvm" "git-extras"
              "fonts" "tpm" "nvm" "rvm" "antibody"
              "scm_breeze" "tmux-themepack" "rust"
             )
