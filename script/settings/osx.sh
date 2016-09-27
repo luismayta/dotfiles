95c8bea645743e554f46c0afbe8a650f3c78f349
Luis Mayta <luis@hadenlabs.com>
2016-09-27 16:20:47 -0500

    fix: clean exports vars (#92)

--- script/settings/osx.sh
+++ script/settings/osx.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-export FONTS_DIR="$HOME/Library/Fonts"
-export APPS=("homebrew" "brew" "pyenv" "gvm" "git-extras"
+FONTS_DIR="$HOME/Library/Fonts"
+APPS=("homebrew" "brew" "pyenv" "gvm" "git-extras"
              "fonts" "tpm" "nvm" "rvm" "antibody"
              "scm_breeze" "tmux-themepack" "mac-cli" "rust"
             )
