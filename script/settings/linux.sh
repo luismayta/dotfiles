8d78c819a9192ee0aa9e71085066730c746c86c1
@slovacus <slovacus@gmail.com>
2016-09-27 21:46:22 -0500

    fix: name bootstrap (#96)

--- script/settings/linux.sh
+++ script/settings/linux.sh
@@ -3,7 +3,16 @@
 
 FONTS_DIR="$HOME/.fonts"
 mkdir -p "$FONTS_DIR"
-APPS=("pyenv" "gvm" "git-extras"
-             "fonts" "tpm" "nvm" "rvm" "antibody"
-             "scm_breeze" "tmux-themepack" "rust"
-            )
+APPS=(
+    "pyenv"
+    "gvm"
+    "git-extras"
+    "fonts"
+    "tpm"
+    "nvm"
+    "rvm"
+    "antibody"
+    "scm_breeze"
+    "tmux-themepack"
+    "rust"
+)
