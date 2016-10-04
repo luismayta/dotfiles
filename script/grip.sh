80d37e984b506ce4ebbfb9717b3d10adc5df70b2
Luis Mayta <luis@hadenlabs.com>
2016-10-04 06:02:49 -0500

    chore: implement grip settings (#98)

---
+++ script/grip.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# shellcheck source=script/bootstrap.sh
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+grip --pass "${GITHUB_API_TOKEN}" ${GRIP_PORT}
