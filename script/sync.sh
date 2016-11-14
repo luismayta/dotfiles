d328eab60fc87c1e0529e586f8e9d778b850447d
Luis Mayta <luis@hadenlabs.com>
2016-11-14 09:24:07 -0500

    fix: script bootstrap (#110)

--- script/sync.sh
+++ script/sync.sh
@@ -3,3 +3,10 @@
 
 # shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+echo $(date)
+
+echo 'Progress'
+
+rsync --chmod=ug=rwX -e "ssh -i $KEY_FILE" -axv \
+      $DEPLOY_SOURCE_DIR $DEPLOY_ACCOUNT@$DEPLOY_SERVER:$DEPLOY_DEST_DIR --progress
