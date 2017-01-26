8b19555f575acb277bda73cb3aaf24ea7d599407
Lucho <lucho@hadenlabs.com>
2017-01-26 01:00:51 -0500

    fix: clean script not used (#124)

--- script/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=/dev/null
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
--- script/roles_ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=/dev/null
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-ansible-galaxy install -r requirements.yml \
-               --roles-path "${PROVISION_DIR}"/roles/contrib --force
