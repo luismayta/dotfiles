8b5a458ee5631e96b970bbba6953466c61fb6b10
Luis Mayta <luis@hadenlabs.com>
2017-07-30 22:03:28 -0500

    build: clean files unnecessary (#133)

--- script/grip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=/dev/null
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-grip --pass "${GITHUB_API_TOKEN}" "${GRIP_PORT}"
