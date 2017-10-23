d4d03db55c219afd74765a16e94d73cbc39f554f
Luis Mayta <luis@hadenlabs.com>
2017-10-22 20:02:46 -0500

    chore: implement script for extras (#133)

---
+++ extras/script/settings/osx.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+export FONTS_DIR="${HOME}/Library/Fonts"
+export APPS=(
+    "homebrew"
+    "brew"
+    "pyenv"
+    "gvm"
+    "git-extras"
+    "fonts"
+    "tpm"
+    "nvm"
+    "rvm"
+    "antibody"
+    "scm_breeze"
+    "mac-cli"
+    "rust"
+)
