d50e88b44e6775925a6ab2dd66d8d7961012e6d4
Luis Mayta <luis@hadenlabs.com>
2017-11-21 11:17:28 -0500

    fix: update install brew (#144)

--- tools/brew/install.sh
+++ tools/brew/install.sh
@@ -57,14 +57,16 @@ if ! grep -Fq '/usr/local/bin/bash' /etc/shells; then
 fi;
 
 # developer
-brew install editorconfig
-
-# emacs
-brew install emacs --with-cocoa
-brew linkapps emacs
-
-# silver searcher
 brew install the_silver_searcher
+brew install editorconfig
+brew install aspell --with-lang-en
+
+# for the GNU global tag system. Used by ggtags.
+brew install --HEAD ctags
+brew install global --with-ctags
+# program used for plantuml
+brew install graphviz
+brew install markdown
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
