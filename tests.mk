906920915124abe84de1ca77401868f1dd2152c0
@slovacus <slovacus@gmail.com>
2018-01-01 11:49:03 -0500

    !#148 Changed: actions for makefile

--- tests.mk
+++ tests.mk
@@ -2,6 +2,17 @@
 # See ./CONTRIBUTING.rst
 #
 
+test.help:
+	@echo '    Tests:'
+	@echo ''
+	@echo '        test                      Run all test'
+	@echo '        test.lint                 Run all pre-commit'
+	@echo '        test.syntax               Run all syntax in code'
+	@echo ''
+
+test: clean
+	@echo $(MESSAGE) Running tests $(END)
+
 test.lint: clean
 	pre-commit run --all-files --verbose
 
