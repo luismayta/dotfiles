325012fa3b11ce04ec276b1459bd1656e54e20c8
@slovacus <slovacus@gmail.com>
2018-12-16 10:03:40 -0500

    fix: run docker for actions (1.3.0)

--- extras/make/docs.mk
+++ extras/make/docs.mk
@@ -3,19 +3,19 @@
 #
 
 FILE_README=$(ROOT_DIR)/README.rst
+PATH_DOCKER_COMPOSE:=provision/docker-compose
+
+docs: docs.help
 
 docs.help:
 	@echo '    Docs:'
 	@echo ''
 	@echo '        docs.show                  Show restview README'
-	@echo '        docs.make.html             Make documentation html'
+	@echo '        docs.make                  Make documentation html'
 	@echo ''
 
 docs.show: clean
 	restview "${FILE_README}"
 
-docs.make.html: clean
-	docker-compose run --rm docs bash -c "cd docs && make html"
-
-docs.make.pdf: clean
-	docker-compose run --rm docs bash -c "cd docs && make latexpdf"
+docs.make: clean
+	$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml run --rm docs bash -c "cd docs && make html"
