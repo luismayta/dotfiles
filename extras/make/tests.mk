4f4349d4a0ca90ad4847f65ca36823076a4f9dae
Lucho <lucho@hadenlabs.com>
2019-02-10 07:04:11 -0500

    chore: actions for test make (#159)

--- extras/make/tests.mk
+++ extras/make/tests.mk
@@ -1,17 +1,29 @@
 #
 # See ./CONTRIBUTING.rst
 #
+PATH_DOCKER_COMPOSE:=provision/docker-compose
+DOCKER_TEST := docker-compose -f docker-compose.yml -f "${PATH_DOCKER_COMPOSE}"/test.yml
 
 test.help:
 	@echo '    Tests:'
 	@echo ''
-	@echo '        test                      Run all test'
+	@echo '        test                      show help test'
+	@echo '        test.all                  Run all test'
+	@echo '        test.run                  Run module test'
 	@echo '        test.lint                 Run all pre-commit'
 	@echo '        test.syntax               Run all syntax in code'
 	@echo ''
 
 test: clean
-	@echo $(MESSAGE) Running tests $(END)
+	@if [[ -z "${run}" ]]; then \
+		make test.help;\
+	fi
+	@if [[ -n "${run}" ]]; then \
+		$(DOCKER_TEST) run --rm app bash -c "pytest tests/${run}";\
+	fi
+
+test.all: clean
+	$(DOCKER_TEST) run --rm app bash -c "pytest "
 
 test.lint: clean
 	pre-commit run --all-files --verbose
