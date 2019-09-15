88dd90de3e92266dab3d287f64ceb784e0abcfaa
@slovacus <slovacus@gmail.com>
2019-09-15 06:06:41 -0500

    chore: actions test for make (#184)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -1,5 +1,3 @@
-docker_test=$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml
-
 test.help:
 	@echo '    Tests:'
 	@echo ''
@@ -10,7 +8,7 @@ test.help:
 	@echo '        test.lint                 Run all pre-commit'
 	@echo '        test.lint.docker          Run all pre-commit in docker'
 	@echo '        test.syntax               Run all syntax in code'
-	@echo '        test.validate             Run all validation fixture dead in code'
+	@echo '        test.validate           Run all validation fixture dead in code'
 	@echo ''
 
 test: clean
@@ -19,26 +17,31 @@ test: clean
 		make test.help;\
 	fi
 	@if [ -n "${run}" ]; then \
-		$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test ${run}";\
+		$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
+			app bash -c "$(PIPENV_RUN) pytest ${run}" ; \
 	fi
 
 test.all: clean
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test"
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
+		app bash -c "$(PIPENV_RUN) pytest"
 
 test.picked: clean
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test --picked"
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
+		app bash -c "$(PIPENV_RUN) pytest --picked"
 
 test.validate: clean
 	@echo $(MESSAGE) Running tests validation fixture $(END)
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test --dead-fixtures"
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
+		app bash -c "$(PIPENV_RUN) pytest --dead-fixtures"
 
 test.lint: clean
-	pre-commit run --all-files --verbose
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
+		app bash -c "$(PIPENV_RUN) pre-commit run --all-files --verbose"
 
 test.lint.docker: clean
 	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml \
-		run --rm check sh -c "pipenv run pre-commit run --all-files --verbose"
+		run --rm check sh -c "$(PIPENV_RUN) pre-commit run --all-files --verbose"
 
 test.syntax: clean
 	@echo $(MESSAGE) Running tests $(END)
