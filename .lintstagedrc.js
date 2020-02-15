b23e66ddc3d3f9ec992af72e3c4d21080407eed9
Lucho <lucho@hadenlabs.com>
2020-02-14 20:10:27 -0500

    refactor: settings project (#204)

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const escape = require('shell-quote').quote;
+
+module.exports = {
+  '*.{ts,tsx,js,json,css}': (filenames) => [
+    ...filenames.map((filename) => `prettier --check "${escape([filename])}"`),
+    ...filenames.map((filename) => `git add "${filename}"`),
+  ],
+  '*.{ts,tsx,js,jsx}': ['eslint'],
+  '*.{ts,tsx,css}': ['stylelint'],
+};
