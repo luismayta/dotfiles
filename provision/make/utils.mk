b37d95d0b4459f53d008341a3690d92968e11f06
Lucho <lucho@hadenlabs.com>
2020-04-26 23:19:52 -0500

    chore: implement actions for make with keybase (#218)

--- provision/make/utils.mk
+++ provision/make/utils.mk
@@ -1,3 +1,27 @@
 #
 # See ./CONTRIBUTING.rst
 #
+.PHONY: utils.help
+
+utils.help:
+	@echo '    utils:'
+	@echo ''
+	@echo '        utils                      help utils'
+	@echo '        utils.generate             generate key ssh by stage'
+	@echo ''
+
+utils:
+	make utils.help
+
+utils.generate:
+	mkdir -p ${KEYBASE_PATH}/${stage}/{pem,pub,private,openssl}
+	mkdir -p ${KEYBASE_PATH}/${stage}/pub
+	mkdir -p ${KEYBASE_PATH}/${stage}/private
+	mkdir -p ${KEYBASE_PATH}/${stage}/openssl
+
+	ssh-keygen -q -m PEM -t rsa -b 4096 -C "admin@${PROJECT}-${stage}.com" -f ${PROJECT}-${stage} -P ""
+	openssl rsa -in ${PROJECT}-${stage} -outform pem > ${PROJECT}-${stage}.pem
+	chmod 0600 ${PROJECT}-${stage}.pem
+	cp -rf ${PROJECT}-${stage}.pem ${KEYBASE_PATH}/${stage}/pem/
+	cp -rf ${PROJECT}-${stage}.pub ${KEYBASE_PATH}/${stage}/pub/
+	cp -rf ${PROJECT}-${stage} ${KEYBASE_PATH}/${stage}/private/
