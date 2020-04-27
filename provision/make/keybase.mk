c50b70cacd3c3db7fa1254bc3c17dd5f4f3823dd
@slovacus <slovacus@gmail.com>
2020-04-26 23:19:52 -0500

    chore: implement actions for make with keybase (#218)

---
+++ provision/make/keybase.mk
@@ -0,0 +1,19 @@
+#
+# See ./CONTRIBUTING.rst
+#
+.PHONY: keybase.help
+
+keybase.help:
+	@echo '    keybase:'
+	@echo ''
+	@echo '        keybase                      help keybase'
+	@echo '        keybase.setup                Setup dependences for keybase'
+	@echo ''
+
+keybase:
+	make keybase.help
+
+keybase.setup:
+	@echo "=====> make dependences for ${TEAM}..."
+	mkdir -p ${KEYBASE_PATH}/{staging,prod,dev}/{pem,private,password,pub,openssl}
+	@echo ${MESSAGE_HAPPY}
