07e5cda416c728576aa7ed6cbf97a5128b8062c5
@slovacus <slovacus@gmail.com>
2020-07-27 11:13:57 -0500

    refactor: actions for make (#239)

---
+++ provision/make/yarn.mk
@@ -0,0 +1,49 @@
+#
+# See ./CONTRIBUTING.rst
+#
+yarn.help:
+	@echo '    yarn:'
+	@echo ''
+	@echo '        yarn                       command=(build|dev|start|export)'
+	@echo '        yarn.setup                 Install dependences of project'
+	@echo '        yarn.install               Install dependences'
+	@echo '        yarn.dev                   dev project'
+	@echo '        yarn.start                 run project'
+	@echo '        yarn.export                export project'
+	@echo '        yarn.build                 build or with stage=(prod)'
+	@echo ''
+
+# setup download and install dependence.
+.PHONY: yarn.setup
+yarn.setup:
+	@echo "=====> setup dependence yarn..."
+	yarn install
+	@echo ${MESSAGE_HAPPY}
+
+yarn.install:
+	$(docker-yarn-run) yarn install
+
+yarn.start:
+	$(docker-yarn-run) yarn start
+
+yarn.build:
+	@if [ -z "${stage}" ]; then \
+		$(docker-yarn-run) yarn build; \
+	else \
+		$(docker-yarn-run) yarn build:${stage}; \
+	fi
+
+yarn.dev:
+	$(docker-yarn-run) yarn dev
+
+yarn.export:
+	$(docker-yarn-run) yarn export
+
+yarn:
+	@if [ -z "${command}" ]; then \
+		make yarn.help;\
+	fi
+	@if [ -n "${command}" ]; then \
+		mkdir -p public;\
+		$(docker-yarn-run) yarn ${command};\
+	fi
