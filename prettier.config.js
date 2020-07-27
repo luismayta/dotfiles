4aed553c566d16d2f39ad497a7815e65854fae1c
@slovacus <slovacus@gmail.com>
2020-07-27 11:16:53 -0500

    chore: implement refactor package for node (#239)

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,58 @@
-module.exports = require('@equipindustry/prettierrc');
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: false,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  singleQuote: true,
+  overrides: [
+    {
+      files: '{*.js?(on),*.y?(a)ml,.*.js?(on),.*.y?(a)ml,*.md,.prettierrc,.stylelintrc,.babelrc}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json,.stylelintrc}',
+      options: {
+        parser: 'json',
+        quoteProps: 'preserve',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 100,
+        proseWrap: 'never',
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.mdx',
+      options: {
+        printWidth: 100,
+        proseWrap: 'never',
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.{sass,scss}',
+      options: {
+        parser: 'scss',
+      },
+    },
+    {
+      files: '*.less',
+      options: {
+        tabWidth: 4,
+        printWidth: 80,
+        parser: 'less',
+      },
+    },
+  ],
+}
