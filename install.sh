efbd294cc5ac675aaa45339e57acccdb2fd964bb
Lucho <lucho@hadenlabs.com>
2021-01-15 23:27:40 -0500

    chore: implement install dependences for osx and arch (#213)

--- install.sh
+++ install.sh
@@ -6,16 +6,14 @@ APP_NAME='.dotfiles'
 GIT_URI='https://github.com/luismayta/dotfiles.git'
 GIT_BRANCH='master'
 DEBUG_MODE='0'
-PATH_REPO="$HOME/$APP_NAME"
-PATH_BACKUP="$HOME/backup"
-FILES_FONTS="$PATH_REPO/resources/fonts/*"
+PATH_REPO="${HOME}/${APP_NAME}"
 
 msg() {
     printf '%b\n' "$1" >&2
 }
 
 success() {
-    if [ "$ret" -eq '0' ]; then
+    if [[ "$ret" -eq '0' ]]; then
         msg "\e[32m[✔]\e[0m ${1}${2}"
     fi
 }
@@ -31,35 +29,8 @@ debug() {
     fi
 }
 
-############################  BASIC SETUP TOOLS
-program_exists() {
-    local $app=$1
-    local message="Need to install $app."
-    local ret='0'
-    type $1 >/dev/null 2>&1 || { local ret='1'; }
-
-    # throw error on non-zero return value
-    if [ ! "$ret" -eq '0' ]; then
-        error "$message"
-        exit
-    fi
-}
-
-upgrade_repo() {
-      msg "trying to update $1"
-
-      if [ "$1" = "${APP_NAME}" ]; then
-        cd "$PATH_REPO" || exit
-        git pull origin "${GIT_BRANCH}"
-      fi
-
-      ret="$?"
-      success "$2"
-      debug
-}
-
 clone_repo() {
-    if [ ! -e "$PATH_REPO/.git" ]; then
+    if [[ ! -e "${PATH_REPO}/.git" ]]; then
         git clone --recursive -b "$GIT_BRANCH" "$GIT_URI" "$PATH_REPO"
         ret="$?"
         success "$1"
@@ -68,17 +39,66 @@ clone_repo() {
         upgrade_repo "$APP_NAME" "Successfully updated $APP_NAME"
     fi
 
-    if [ "$ret" -eq '0' ]; then
-        "$PATH_REPO/bootstrap.sh"
+    if [[ "$ret" -eq '0' ]] && [[ ! $TRAVIS = 'true' ]]; then
+        cd "${PATH_REPO}" || exit
+        make run
     fi
 }
+
+############################  BASIC SETUP TOOLS
+program_exists() {
+    local app=$1
+    local message="Need to install $app."
+    local ret='0'
+    type "$1" >> /dev/null 2>&1 || { local ret='1'; }
+
+    # throw error on non-zero return value
+    if [[ ! "$ret" -eq '0' ]]; then
+        error "$message"
+        exit
+    fi
+}
+
+function upgrade_repo() {
+    msg "trying to update $1"
+
+    if [ "$1" = "${APP_NAME}" ]; then
+        cd "$PATH_REPO" || exit
+        git pull origin "${GIT_BRANCH}"
+    fi
+
+    ret="$?"
+    success "$2"
+    debug
+}
+
+# Mac Stuff -------------------------------------------------------------------
+if [[ $(uname) == 'Darwin' ]]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install zsh git go rsync
+    # Utils -----------------------------------------------------------------------
+    brew install jq ag fd ripgrep cmake ctags
+fi
+
+# Archlinux Stuff
+if type -p pacman > /dev/null; then
+    packages=(
+        go
+        npm
+        yarn
+    )
+    for package in "${packages[@]}"; do
+        pacman -S --noconfirm "${package}"
+    done
+    npm install -g n
+fi
 
 for app in {zsh,git,rsync}; do
     program_exists "$app"
 done
 unset app
 
-clone_repo      "Successfully cloned $app_name"
+clone_repo      "Successfully cloned ${APP_NAME}"
 
-msg             "\nThanks for installing $app_name."
-msg             "© `date +%Y` $app_name"
+msg             "\nThanks for installing ${APP_NAME}."
+msg             "© $(date +%Y) ${APP_NAME}"
