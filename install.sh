cb60cc34d6c03ab09c151be5fcb2ddd056f86592
@slovacus <slovacus@gmail.com>
2021-01-15 20:57:56 -0500

    chore: implement install dotfiles

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 ############################  SETUP PARAMETERS
-APP_NAME='.dotfiles'
-GIT_URI='https://github.com/luismayta/dotfiles.git'
-GIT_BRANCH='master'
-DEBUG_MODE='0'
-PATH_REPO="${HOME}/${APP_NAME}"
+app_name='.dotfiles'
+git_uri='https://github.com/luismayta/dotfiles.git'
+debug_mode='0'
+fork_maintainer='0'
+path_repo="$HOME/$app_name"
 
 msg() {
     printf '%b\n' "$1" >&2
 }
 
 success() {
-    if [[ "$ret" -eq '0' ]]; then
-        msg "\e[32m[✔]\e[0m ${1}${2}"
+    if [ "$ret" -eq '0' ]; then
+    msg "\e[32m[✔]\e[0m ${1}${2}"
     fi
 }
 
@@ -24,22 +24,19 @@ error() {
 }
 
 debug() {
-    if [ "$DEBUG_MODE" -eq '1' ] && [ "$ret" -gt '1' ]; then
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
       msg "An error occured in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
 
 ############################  BASIC SETUP TOOLS
 program_exists() {
-    local app=$1
-    local message="Need to install $app."
     local ret='0'
-    type "$1" >> /dev/null 2>&1 || { local ret='1'; }
+    type $1 >/dev/null 2>&1 || { local ret='1'; }
 
     # throw error on non-zero return value
-    if [[ ! "$ret" -eq '0' ]]; then
-        error "$message"
-        exit
+    if [ ! "$ret" -eq '0' ]; then
+        error "$2"
     fi
 }
 
@@ -57,18 +54,13 @@ function upgrade_repo() {
 }
 
 clone_repo() {
-    if [[ ! -e "${PATH_REPO}/.git" ]]; then
-        git clone --recursive -b "$GIT_BRANCH" "$GIT_URI" "$PATH_REPO"
+    if [ ! -e "$path_repo/.git" ]; then
+        git clone --recursive -b "$git_branch" "$git_uri" "$path_repo"
         ret="$?"
         success "$1"
         debug
     else
-        upgrade_repo "$APP_NAME" "Successfully updated $APP_NAME"
-    fi
-
-    if [[ "$ret" -eq '0' ]] && [[ ! $TRAVIS = 'true' ]]; then
-        cd "${PATH_REPO}" || exit
-        make run
+        upgrade_repo "$app_name" "Successfully updated $app_name"
     fi
 }
 
@@ -77,7 +69,9 @@ for app in {zsh,git,tmux}; do
 done
 unset app
 
-clone_repo      "Successfully cloned ${APP_NAME}"
+clone_repo      "Successfully cloned $app_name"
 
-msg             "\nThanks for installing ${APP_NAME}."
-msg             "© $(date +%Y) ${APP_NAME}"
+msg             "\nThanks for installing $app_name."
+msg             "© `date +%Y` $app_name"
+
+"$path_repo/bootstrap.sh"
