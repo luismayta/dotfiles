7fef0f34313e8c0a9e03aa902072e88ada78cea6
Luis Mayta <luis@hadenlabs.com>
2021-03-15 17:35:33 -0500

    chore: implement reviewers action (#270)

--- provision/make/git.mk
+++ provision/make/git.mk
@@ -6,8 +6,9 @@ git.help:
 	@echo '    git:'
 	@echo ''
 	@echo '        git                 show help'
-	@echo '        git.setup           install dependences to application'
+	@echo '        git.setup            install dependences to application'
 	@echo '        git.ignore           install dependences to application'
+	@echo '        git.reviews          add revieweers to git config'
 	@echo ''
 
 git:
@@ -21,10 +22,16 @@ git:
 git.setup:
 	@echo "==> setup git..."
 	make git.ignore
+	make git.reviews
 
-# setup all actions git ignore.
 .PHONY: git.ignore
 git.ignore:
 	@echo "==> git ignore generated..."
 	@$(GI) ${GIT_IGNORES} > .gitignore
+	@echo ${MESSAGE_HAPPY}
+
+.PHONY: git.reviews
+git.reviews:
+	@echo "==> add issues revievers..."
+	@git config github.reviews "${REVIEWERS}"
 	@echo ${MESSAGE_HAPPY}
