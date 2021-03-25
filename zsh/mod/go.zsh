ab4a3f98f144888be30dce67ab6d5576f4140ba3
Luis Mayta <luis@hadenlabs.com>
2021-03-25 01:39:38 -0500

    refactor: mod files zsh (#286)

--- zsh/mod/go.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ksh
-# -*- coding: utf-8 -*-
-
-export GOPATH="${HOME}/Projects"
-export PATH="${PATH}":"${GOPATH}"/bin
-export GO111MODULE=on
---
+++ zsh/mod/internal/osx.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+# create cache and reload settings
+function reload {
+    exec "${SHELL}" -l
+}
