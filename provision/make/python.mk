2dc5161481276b3f9d2b870b65af911e406bb442
Lucho <lucho@hadenlabs.com>
2021-04-29 19:34:21 -0500

    ci: implement action make python

--- provision/make/python.mk
+++ provision/make/python.mk
@@ -1,7 +1,5 @@
-# python
+# python show help commands.
 .PHONY: python.help
-
-
 python.help:
 	@echo '    python:'
 	@echo ''
@@ -13,37 +11,40 @@ python.help:
 	@echo '        python.setup           install dependences to application'
 	@echo ''
 
+## Show commands python
+.PHONY: python
 python:
 	@if [ -z "${command}" ]; then \
 		make python.help;\
 	fi
 
-
+## Run linter
 .PHONY: python.lint
-python.lint: ## Run linter
+python.lint:
 	@echo "==> lint python..."
 	@echo ${MESSAGE_HAPPY}
 
+## Fix lint violations
 .PHONY: python.fix
-python.fix: ## Fix lint violations
+python.fix:
 	@echo "==> fix python..."
 	@echo ${MESSAGE_HAPPY}
 
-# setup download and install dependence.
+## setup download and install dependence.
 .PHONY: python.setup
 python.setup:
 	@echo "==> setup python..."
 	$(PIPENV_INSTALL) --dev --skip-lock
 	@echo ${MESSAGE_HAPPY}
 
-# environment make for python.
+## environment make for python.
 .PHONY: python.environment
 python.environment:
 	@echo "==> environment python..."
 	pipenv --venv || $(PIPENV_INSTALL) --skip-lock
 	@echo ${MESSAGE_HAPPY}
 
-# python pre-commit make for python.
+## python pre-commit make for python.
 .PHONY: python.precommit
 python.precommit:
 	@echo "==> install hooks for pre-commit..."
