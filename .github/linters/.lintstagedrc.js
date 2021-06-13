7e2df884a6fcfead0efd9f8770e2d20e24e1b846
@slovacus <slovacus@gmail.com>
2021-06-12 20:10:58 -0500

    refactor: lint validation code (#294)

--- .github/linters/.lintstagedrc.js
+++ .github/linters/.lintstagedrc.js
@@ -3,8 +3,8 @@ const escape = require('shell-quote').quote
 module.exports = {
   '*.{ts,tsx,js,json,css}': (filenames) => [
     ...filenames.map((filename) => `prettier --check "${escape([filename])}"`),
-    ...filenames.map((filename) => `git add "${filename}"`),
+    ...filenames.map((filename) => `git add "${filename}"`)
   ],
   '*.{ts,tsx,js,jsx}': ['eslint'],
-  '*.{ts,tsx,css}': ['stylelint'],
+  '*.{ts,tsx,css}': ['stylelint']
 }
