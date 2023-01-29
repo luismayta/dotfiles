60d0182d8e88a0855dbe4afc76f2410633081d3b
Lucho <lucho@hadenlabs.com>
2023-01-28 20:30:16 -0500

    chore: implement custom config hammerspoon (#377)
    * feat: upgrade hooks pre-commit (#377)
    
    * chore: implement load hammerspoon custom config (#377)
    
    * fix: change load branch with name develop to main (#377)
    
    * feat: implement argument no cache for build docker (#377)
    
    * build: generate files (#377)

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ Using the given version number of `MAJOR.MINOR.PATCH`, we apply the following co
 
 ## Copyright
 
-Copyright © 2018-2022 [Hadenlabs](https://hadenlabs.com)
+Copyright © 2018-2023 [Hadenlabs](https://hadenlabs.com)
 
 ## Trademarks
 
---
+++ config/hammerspoon/custom.lua
@@ -0,0 +1,42 @@
+-- app environment keybindings. Bundle `id` is preferred, but application `name` will be ok using cmd + ctrl + alt.
+local config = {}
+config.devs = {
+  { key = "b", name = "Bitwarden" },
+  { key = "c", name = "Vscodium" },
+  { key = "d", name = "DBeaver" },
+  { key = "e", name = "Emacs" },
+  { key = "t", name = "Kitty" },
+  { key = "n", name = "Notion" },
+  { key = "x", name = "XCode" },
+  { key = "i", name = "Insomnia" },
+  { key = "k", name = "Keybase" },
+  { key = "s", name = "Slack" },
+  { key = "j", name = "Jira" },
+  { key = "m", name = "Miro" },
+  { key = "o", name = "Obsidian" },
+  { key = "z", name = "Zoom.us" },
+}
+
+-- app environment keybindings. Bundle `id` is preferred, but application `name` will be ok using ctrl + alt.
+config.apps = {
+  { key = ",", name = "System Preferences" },
+  { key = "3", name = "Launchpad" },
+  { key = "d", name = "Discord" },
+  { key = "f", name = "Figma" },
+  { key = "e", name = "Spark" },
+  { key = "a", name = "Airtable" },
+  { key = "b", name = "Brave Browser" },
+  { key = "c", name = "GoogleCalendar" },
+  { key = "h", name = "WhatsApp" },
+  { key = "t", name = "Telegram" },
+  { key = "m", name = "Spotify" },
+  { key = "r", name = "Reminders" },
+  { key = "o", name = "Finder" },
+}
+
+-- misc environment keybindings. Bundle `id` is preferred, but application `name` will be ok using shift + cmd.
+config.misc = {
+  { key = "b", name = "Binance" },
+}
+
+return config
