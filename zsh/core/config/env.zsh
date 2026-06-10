5caf5ed9f0bcc3731d0689d1d4ee04a9ebc7291b
Lucho <lucho@hadenlabs.com>
2026-06-10 13:44:53 -0500

    deprecate ⚰ (core): HAD-62 Remove dead CORE_MESSAGE_YAY, RVM, NVM, PYENV

--- zsh/core/config/env.zsh
+++ zsh/core/config/env.zsh
@@ -4,12 +4,8 @@
 #
 
 export CORE_MESSAGE_BREW="Please install brew or use the brew module at zsh/modules/brew"
-export CORE_MESSAGE_YAY="Please install yay (Arch Linux AUR helper)"
-export CORE_MESSAGE_RVM="Please install rvm or use the rvm module at zsh/modules/rvm"
 export CORE_MESSAGE_RUST="Please install rust or use the rust module at zsh/modules/rust"
-export CORE_MESSAGE_NVM="Please install nvm or use the fnm module at zsh/modules/fnm"
 export CORE_MESSAGE_CARGO="Please install cargo or use the rust module at zsh/modules/rust"
-export CORE_MESSAGE_PYENV="Please install pyenv or use the pyenv module at zsh/modules/pyenv"
 export CORE_PROJECTS_BACKUP_PATH="${HOME}/backup"
 
 export ANDROID_HOME="${HOME}/Library/Android/sdk"
