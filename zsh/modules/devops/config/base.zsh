e6ebae68ddc1c3e048898b2e73e182130f7eafdc
Lucho <lucho@hadenlabs.com>
2026-06-10 07:43:02 -0500

    refactor 🎨 (devops): HAD-62 remove duplicate DEVOPS_MESSAGE_BREW

--- zsh/modules/devops/config/base.zsh
+++ zsh/modules/devops/config/base.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export DEVOPS_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew"
 export DEVOPS_PACKAGE_NAME=devops
 
 export DEVOPS_TOOLS=(
