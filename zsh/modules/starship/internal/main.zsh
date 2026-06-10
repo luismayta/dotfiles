4151869573bed7e2c134350e4d2d6f4b3ca832ca
Lucho <lucho@hadenlabs.com>
2026-06-10 06:52:03 -0500

    fix 🐛 (starship): HAD-62 fix shellcheck SC1091

--- zsh/modules/starship/internal/main.zsh
+++ zsh/modules/starship/internal/main.zsh
@@ -1,13 +1,16 @@
 # shellcheck shell=bash
 # Starship internal main loader
 
+# shellcheck source=/dev/null
 source "${ZSH_STARSHIP_PATH}/internal/base.zsh"
 
 case "${OSTYPE}" in
   darwin*)
+    # shellcheck source=/dev/null
     source "${ZSH_STARSHIP_PATH}/internal/osx.zsh"
     ;;
   linux*)
+    # shellcheck source=/dev/null
     source "${ZSH_STARSHIP_PATH}/internal/linux.zsh"
     ;;
 esac
