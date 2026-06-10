41069c3e77961a775d28d2472db3ffebf0a7fa7c
Lucho <lucho@hadenlabs.com>
2026-06-10 18:09:36 -0500

    fix 🐛 (zsh): HAD-61 Fix typo neccesary to necessary in ssh alias

--- zsh/modules/ssh/pkg/alias.zsh
+++ zsh/modules/ssh/pkg/alias.zsh
@@ -4,7 +4,7 @@ if core::exists assh; then alias ssh="assh wrapper ssh"; fi
 
 function editassh {
     if [ -z "${EDITOR}" ]; then
-        message_warning "it's neccesary the var EDITOR"
+        message_warning "it's necessary the var EDITOR"
         return
     fi
     "${EDITOR}" "${ASSH_FILE_SETTINGS}"
