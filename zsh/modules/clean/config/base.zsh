aad0be86ad3a57e0964b009840aae71efceb377b
Lucho <lucho@hadenlabs.com>
2026-06-10 07:42:59 -0500

    refactor 🎨 (clean): HAD-62 remove duplicate CLEAN_MESSAGE vars

--- zsh/modules/clean/config/base.zsh
+++ zsh/modules/clean/config/base.zsh
@@ -1,7 +1,5 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export CLEAN_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew"
-export CLEAN_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm"
 export CLEAN_MESSAGE_NOT_IMPLEMENTED="Please this not implement add issue to luismayta/zsh-clean"
 export CLEAN_PACKAGE_NAME=clean
