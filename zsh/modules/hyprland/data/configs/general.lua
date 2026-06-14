2b75b6e25136ddf63ec28965b2826e046b05ad96
Lucho <luch@hadenlabs.com>
2026-06-14 10:47:59 -0500

    feat ✨ (hyprland): HAD-61 Add touchpad input configuration

--- zsh/modules/hyprland/data/configs/general.lua
+++ zsh/modules/hyprland/data/configs/general.lua
@@ -38,5 +38,15 @@ hl.config({
     input = {
         repeat_rate = 35,
         repeat_delay = 200,
+        -- Trackpad / libinput settings for Bluetooth trackpad (Magic Trackpad)
+        touchpad = {
+            natural_scroll = true,            -- macOS-style: content follows finger direction
+            tap_to_click = true,               -- Tap to left-click
+            clickfinger_behavior = true,       -- 2-finger = right-click, 3-finger = middle-click
+            scroll_factor = 0.5,               -- Slower scroll for finer control
+            disable_while_typing = true,       -- Prevent palm activation while typing
+            middle_button_emulation = false,   -- Handled by clickfinger_behavior instead
+            drag_lock = true,                  -- Lift finger during drag without dropping
+        },
     },
 })
