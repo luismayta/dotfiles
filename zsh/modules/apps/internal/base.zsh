36a8d5ecd9ac271be25f87ca9d103106f96c3fab
Lucho <luch@hadenlabs.com>
2026-06-14 17:56:50 -0500

    refactor 🎨 (apps): HAD-61 use core::ensure for idempotent package install

--- zsh/modules/apps/internal/base.zsh
+++ zsh/modules/apps/internal/base.zsh
@@ -3,7 +3,7 @@
 apps::internal::packages::install() {
     message_info "Installing required ${APPS_PACKAGE_NAME} packages"
     for package in "${APPS_PACKAGES[@]}"; do
-        core::install "${package}"
+        core::ensure "${package}"
     done
     message_success "Installed required ${APPS_PACKAGE_NAME} packages"
 }
