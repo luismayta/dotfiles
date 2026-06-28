cf7892839cbfdfd52b23d358ec38d3530ac2f8a3
Lucho <luch@hadenlabs.com>
2026-06-28 10:19:56 -0500

    feat ✨ (mobile): HAD-61 add android package definitions for linux
    Signed-off-by: Lucho <luch@hadenlabs.com>

--- zsh/modules/mobile/config/linux.zsh
+++ zsh/modules/mobile/config/linux.zsh
@@ -2,3 +2,7 @@
 # Linux-specific mobile configuration
 
 export FLUTTER_FILE="linux/flutter_linux_${FLUTTER_VERSION}.tar.xz"
+
+# Android packages managed via core::ensure (Linux/AUR names)
+# shellcheck disable=SC2034 # referenced from internal/main.zsh
+APPS_ANDROID=(android-studio android-sdk-platform-tools)
