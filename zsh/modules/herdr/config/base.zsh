3784dacaef2dfb71af4ffd400d0302620803ae49
Lucho <luch@hadenlabs.com>
2026-07-05 07:33:12 -0500

    feat ✨ (herdr): HAD-61 migrate data prefix to data/conf and align keybindings with tmux
    Signed-off-by: Lucho <luch@hadenlabs.com>

--- zsh/modules/herdr/config/base.zsh
+++ zsh/modules/herdr/config/base.zsh
@@ -4,7 +4,7 @@ ZSH_HERDR_ENABLED="${ZSH_HERDR_ENABLED:-true}"
 export HERDR_PACKAGE_NAME=herdr
 export HERDR_INSTALL_URL="https://herdr.dev/install.sh"
 export HERDR_CONFIG_PATH="${HOME}/.config/herdr"
-export ZSH_HERDR_DATA_PATH="${ZSH_HERDR_PATH}/data"
+export ZSH_HERDR_DATA_PATH="${ZSH_HERDR_PATH}/data/conf"
 
 # Workspace management
 export HERDR_WORKSPACE_PREFIX="${HERDR_WORKSPACE_PREFIX:-}"
