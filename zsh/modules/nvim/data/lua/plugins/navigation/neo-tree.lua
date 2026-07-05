c62ea0dcfd5f5fc21f10d6729b2b1468348147fb
Lucho <luch@hadenlabs.com>
2026-07-05 05:40:51 -0500

    feat ✨ (nvim): HAD-61 add neo-tree explorer keymaps
    Signed-off-by: Lucho <luch@hadenlabs.com>

--- zsh/modules/nvim/data/lua/plugins/navigation/neo-tree.lua
+++ zsh/modules/nvim/data/lua/plugins/navigation/neo-tree.lua
@@ -1,5 +1,23 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute { toggle = true, dir = LazyVim.root() }
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute { toggle = true, dir = vim.uv.cwd() }
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
   opts = {
     filesystem = {
       filtered_items = {
