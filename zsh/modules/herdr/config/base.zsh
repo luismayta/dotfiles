66d5e9fe8bf9b422c76dc4a6fac739e691c20570
Lucho <luch@hadenlabs.com>
2026-07-05 08:31:34 -0500

    refactor 🎨 (herdr): HAD-61 migrate herdr data path from data/conf to data
    Signed-off-by: Lucho <luch@hadenlabs.com>

--- zsh/modules/herdr/config/base.zsh
+++ zsh/modules/herdr/config/base.zsh
@@ -4,7 +4,7 @@ ZSH_HERDR_ENABLED="${ZSH_HERDR_ENABLED:-true}"
 export HERDR_PACKAGE_NAME=herdr
 export HERDR_INSTALL_URL="https://herdr.dev/install.sh"
 export HERDR_CONFIG_PATH="${HOME}/.config/herdr"
-export ZSH_HERDR_DATA_PATH="${ZSH_HERDR_PATH}/data/conf"
+export ZSH_HERDR_DATA_PATH="${ZSH_HERDR_PATH}/data"
 
 # Workspace management
 export HERDR_WORKSPACE_PREFIX="${HERDR_WORKSPACE_PREFIX:-}"
